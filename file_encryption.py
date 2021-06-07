c58dd2cf63d74f853564c9d543b3bcab768f36fd
iamscratches <35376376+iamscratches@users.noreply.github.com>
2021-06-07 22:33:45 +0530

    working model

--- file_encryption.py
+++ file_encryption.py
@@ -5,15 +5,15 @@ Created on Mon Jun  7 13:10:28 2021
 @author: subhankar
 """
 
-from RSA2 import *
+import scratch_encrypt as se
 import time
 import os
 
-reading_units = 1024
+reading_units = 512
 
 #       ENCRYPTION
 
-def encrypt_file(filename):
+def encrypt_file(filename, scratches):
     total_encrypted = 0
     
     file_size = os.path.getsize('files/' + filename) 
@@ -23,11 +23,12 @@ def encrypt_file(filename):
     
     print('Size of file is', file_size, 'bytes')
     
+    
     read_by = reading_units * 4        # read 64 bytes of data from the file one by one
     byte = file.read(read_by)
     
     while byte:
-        encrypted_data = RSAencrypt(byte)
+        encrypted_data = scratches.encrypt(byte)
         total_encrypted += read_by
         # print(len(encrypted_data))
         for data in encrypted_data:
@@ -45,7 +46,7 @@ def encrypt_file(filename):
 
 #       DECRYPTION
 
-def decrypt_file(filename):
+def decrypt_file(filename, scratches):
     total_encrypted = 0
     
     file_size = os.path.getsize('encrypted_files/' + filename) 
@@ -58,7 +59,7 @@ def decrypt_file(filename):
     byte = encrypted_file.read(read_by)
     
     while byte:
-        decrypted_data = RSAdecrypt2(byte)
+        decrypted_data = scratches.decrypt(byte)
         total_encrypted += read_by
         for data in decrypted_data:
             decrypted_file.write(data)
@@ -80,15 +81,47 @@ def check_files(path1, path2):
     file_read = file.read()
     decrypted_file_read = decrypted_file.read()
     if(file_read == decrypted_file_read):
-        print("It's equal")
+        print("Encryption Decryption successful")
     else:
-        print("It's not equal")
+        print("Files didn't match...Encryption or Decryption went wrong check the logs")
         
     file.close()
     decrypted_file.close()
 
-start_time = time.time()
-encrypted_filename = encrypt_file("image2.jpg")
-decrypted_filename = decrypt_file(encrypted_filename)
-check_files("files/text.txt","decrypted_files/" + decrypted_filename)
-print(f'execution completes in {(time.time() - start_time)} seconds')
+filename = input("Enter filename with extension: ")
+generator = input("Press 'G' for generating a new key or just press enter to enter ur keys: ")
+if not generator == 'G':
+    choice = input("'E' for encryption, 'D' for decryption, 'B' for both or 'C' for check: ")
+    if choice == 'E':
+        E = int(input("Enter public key : "))
+        N = int(input("Enter universal key: "))
+        start_time = time.time()
+        scratches = se.scratch_encrypt(e=E, n=N, generate_key=False)
+        encrypted_filename = encrypt_file(filename, scratches)
+    elif choice == 'D':
+        D = int(input("Enter private key : "))
+        N = int(input("Enter universal key: "))
+        start_time = time.time()
+        scratches = se.scratch_encrypt(d=D, n=N, generate_key=False)
+        decrypted_filename = decrypt_file(filename, scratches)
+    elif choice == 'B':
+        E = int(input("Enter public key : "))
+        E = int(input("Enter private key : "))
+        N = int(input("Enter universal key: "))
+        start_time = time.time()
+        scratches = se.scratch_encrypt(e=E, n=N, d=D, generate_key=False)
+        encrypted_filename = encrypt_file(filename, scratches)
+        decrypted_filename = decrypt_file(encrypted_filename, scratches)
+    elif choice == 'C':
+        decrypted_filename = input("Enter decrypted filename with extension: ")
+        start_time = time.time()
+        check_files("files/" + filename,"decrypted_files/" + decrypted_filename)
+else:
+    Level = int(input("Enter level of encryption u want: "))
+    start_time = time.time()
+    scratches = se.scratch_encrypt(level = Level)
+    encrypted_filename = encrypt_file(filename, scratches)
+    decrypted_filename = decrypt_file(encrypted_filename, scratches)
+    print("Checking validity of the files: ", end="")
+    check_files("files/" + filename,"decrypted_files/" + decrypted_filename)
+print(f'execution completed in {(time.time() - start_time)} seconds')
--- scratch_encrypt.py
+++ scratch_encrypt.py
@@ -8,86 +8,94 @@ Created on Sat Jun  5 14:04:01 2021
 import sympy
 import math
 
-def generateKey():    
-    return sympy.randprime(300, 3000),sympy.randprime(300, 3000)
-
-def generatePair():
-    i = 1
-    while(True):        
-        print("checkpoint 2", (fi*i+1))
-        factors = get_factors(fi*i+1)
-        print("checkpoint 3", factors)
-        i += 1
-        if(len(factors)>8):
-            break
+class scratch_encrypt:
+    def __init__(self, e = -1, d = -1, n = -1, generate_key = True, level = 3):
+        if(generate_key):
+            generatedPrime = self.generatePrime(level)
+            P=generatedPrime[0]
+            Q=generatedPrime[1]
+            self.n = P*Q
+            n = self.n
+            self.fi = (P-1)*(Q-1)
+            
+            print("checkpoint 1", P, Q, self.fi, self.n)
+            
+            
+            l = self.generatePair()
+            print("checkpoint 5", l)
+            e = l[0]
+            d = l[1]
+            if((e*d)%self.fi==1):
+                print("checkpoint 6", "successful", e, d)        
         
-    print("checkpoint 4 final", factors)
-    E = factors[int(len(factors) - 2)]
-    D = factors[int(len(factors) - 1)]
-    print("checkpoint 5 final", E, D)
-    if(E<=1 or E>=fi):
-        print("failed")
-        return [0,0]
-    else:
-        return E,D
-
-def get_factors(x):
-   factors = []
-   i = 1
-       
-   while i <= math.sqrt(x):         
-        if (x % i == 0) :
-            factors.append(i)
-            factors.append(int(x/i))
-        if(i%10000==0):
-           print(".", end=(" "))
-        i = i + 1
-   print()
-   return factors
-
-generatedPrime = generateKey()
-P=generatedPrime[0]
-Q=generatedPrime[1]
-n = P*Q
-fi = (P-1)*(Q-1)
-
-print("checkpoint 1", P, Q, fi, n)
-
-# choose a value for e
-# d * e = 1 mod fi => d * 7 = 1 mod 20
-l = generatePair()
-print("checkpoint 5", l)
-e = l[0]
-d = l[1]
-if((e*d)%fi==1):
-    print("checkpoint 6", "successful", e, d)    
-
-public_key = [e,n]
-private_key = [d,n]
-
-def RSAencrypt(plain_text):
-    plain_text = list(plain_text)
-    cipher_text = []
-    for i in plain_text:
-        cipher_text.append(((i**public_key[0])%public_key[1]).to_bytes(4,'little'))
-        # print(cipher_text)
-    return cipher_text
-
-def RSAdecrypt(cipher_text):
-    cipher_text = int.from_bytes(cipher_text, "little")
-    return ((cipher_text**private_key[0])%private_key[1]).to_bytes(1,'little')
-
-def RSAdecrypt2(cipher_text_full):
-    enlist = list(cipher_text_full)
-    i = 0
-    cipher_text_group = []
-    while(i<len(enlist)):    
-        cipher_text_group.append(bytes(bytearray(enlist[i:i+4])))
-        i += 4        
-    # print(cipher_text_group)
+        self.public_key = [e,n]
+        self.private_key = [d,n]
+        self.expand = 4
+        print("Your public and private keys are: ", self.public_key, self.private_key, "Please keep it secure.")
+        
+        
+    def generatePrime(self, level):    
+        return sympy.randprime(3*(10**(level-1)), 3*(10**level)),sympy.randprime(3*(10**(level-1)), 3*(10**level))
     
-    plain_text = []
-    for cipher_text in cipher_text_group:
-        cipher_text = int.from_bytes(cipher_text, "little")
-        plain_text.append(((cipher_text**private_key[0])%private_key[1]).to_bytes(1,'little'))
-    return plain_text
+    def generatePair(self):
+        i = 1
+        while(True):        
+            print("checkpoint 2", (self.fi * i + 1))
+            factors = self.get_factors(self.fi * i + 1)
+            print("checkpoint 3", factors)
+            i += 1
+            if(len(factors)>8):
+                break
+            
+        print("checkpoint 4 final", factors)
+        E = factors[int(len(factors) - 2)]
+        D = factors[int(len(factors) - 1)]
+        print("checkpoint 5 final", E, D)
+        if(E<=1 or E>=self.fi):
+            print("failed")
+            return [0,0]
+        else:
+            return E,D
+    
+    def get_factors(self, x):
+       factors = []
+       i = 1
+           
+       while i <= math.sqrt(x):         
+            if (x % i == 0) :
+                factors.append(i)
+                factors.append(int(x/i))
+            if(i%10000==0):
+               print(".", end=(" "))
+            i = i + 1
+       print()
+       return factors
+    
+    
+    
+    def encrypt(self, plain_text):
+        if(self.public_key[0] == -1 or self.public_key[1] == -1):
+            raise Exception("You need to either generate a key first or provide ur own key to continue")            
+        plain_text = list(plain_text)
+        cipher_text = []
+        for i in plain_text:
+            cipher_text.append(((i**self.public_key[0])%self.public_key[1]).to_bytes(self.expand,'little'))
+            # print(cipher_text)
+        return cipher_text
+        
+    def decrypt(self, cipher_text_full):
+        if(self.private_key[0] == -1 or self.private_key[1] == -1):
+            raise Exception("You need to either generate a key first or provide ur own key to continue")
+        enlist = list(cipher_text_full)
+        i = 0
+        cipher_text_group = []
+        while(i<len(enlist)):    
+            cipher_text_group.append(bytes(bytearray(enlist[i:i+self.expand])))
+            i += self.expand        
+        # print(cipher_text_group)
+        
+        plain_text = []
+        for cipher_text in cipher_text_group:
+            cipher_text = int.from_bytes(cipher_text, "little")
+            plain_text.append(((cipher_text**self.private_key[0])%self.private_key[1]).to_bytes(1,'little'))
+        return plain_text
